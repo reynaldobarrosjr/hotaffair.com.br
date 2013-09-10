082050e9d7aa1857697fb5520eb41c2366a01542
trevobr <contato@trevobr.com>
2013-09-10 17:43:43 -0300

    first commit of new hot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotaffairComBr::Application.config.secret_token = '61ded749d3cd9b945cd6bdc153558ece91d589db2a9579d4cd197aa241c311d49c8ddb80aad262c559e248bd2c7998b0af02474317998e8fe857ed9f6c95a994'
