f321418f36ba5057459e9cf537c3c2ef8b287ef7
trevobr <contato@trevobr.com>
2013-09-13 17:07:19 -0300

    config hotaffair to use brasex email

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ module HotaffairComBr
       :address              => "smtp.gmail.com", 
       :port                 => 587, 
       :domain               => 'gmail.com', 
-      :user_name            => 'admin@hotaffair.com.br', 
+      :user_name            => 'admin@brasex.com.br', 
       :password             => 'crysisnemesis', 
       :authentication       => 'plain', 
       :enable_starttls_auto => true  
